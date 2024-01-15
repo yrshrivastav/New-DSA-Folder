a5e008fbd46fe7188548b79657f9b18f15daf8c2
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-01-16 04:15:56 +0530

    update

--- LeetCodeQ1.java
+++ LeetCodeQ1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class LeetCodeQ1{
     public static void main(String[] args) {
-        int num1[] = {1,3,5,2};
+        int num1[] = {1,3,5,2,4};
         int num2[] = {9,4,7};
 
         int m = num1.length;
@@ -14,10 +14,22 @@ class LeetCodeQ1{
             num1[m] = no2;
             m++;
         }
-        Arrays.sort(num1);
+
+            
+        for(int i=0; i<m; i++){
+            for(int j=i+1; j<m; j++){
+                if(num1[i]>num1[j]){
+                    int temp=num1[i];
+                    num1[i]=num1[j];
+                    num1[j]=temp;
+                }
+            }
+        }
+
         for(int no3 : num1){
             System.out.println("No in num1 are : "+no3);
         }
 
-        }
+
+    }
 }
