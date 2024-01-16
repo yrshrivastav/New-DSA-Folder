e7b6ec12f8a173443fb28c5e28e658728f01e4dd
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-01-17 01:53:28 +0530

    update

--- LeetCodeQ1.java
+++ LeetCodeQ1.java
@@ -1,35 +1,74 @@
-import java.util.Arrays;
-
 class LeetCodeQ1{
-    public static void main(String[] args) {
-        int num1[] = {1,3,5,2,4};
-        int num2[] = {9,4,7};
-
-        int m = num1.length;
-        int n = num2.length;
-        int sizeOfNewArrayNum1 = m+n;
     
-        num1 = Arrays.copyOf(num1, sizeOfNewArrayNum1);
+    public static void merge(int[] num1, int m, int[] num2, int n) {
+        
+        
+        int size = m + n;
+        int i = 0;
+        int num3[] = new int[size];
+        for(int element : num1){
+            if(element==0){
+                continue;
+            }
+            else{
+                num3[i] = element;
+                i++;
+            }
+        }
         for(int no2 : num2){
-            num1[m] = no2;
-            m++;
+            if(no2==0){
+                continue;
+            }
+            else{
+                num3[i] = no2;
+                i++;
+            }
         }
 
-            
-        for(int i=0; i<m; i++){
-            for(int j=i+1; j<m; j++){
-                if(num1[i]>num1[j]){
-                    int temp=num1[i];
-                    num1[i]=num1[j];
-                    num1[j]=temp;
+        num1=num3;
+
+        //sorting array
+        for(int index=0; index<m; index++){
+            if(num1[index]==0){
+                continue;
+            }
+            else{
+                for(int j=index+1; j<m; j++){
+                    if(num1[j]==0){
+                        continue;
+                    }
+                    else{
+                        if(num1[index]>num1[j]){
+                        int temp=num1[index];
+                        num1[index]=num1[j];
+                        num1[j]=temp;
+                        }
+                    }    
                 }
             }
         }
 
         for(int no3 : num1){
-            System.out.println("No in num1 are : "+no3);
+            if(no3==0){
+                continue;
+            }
+            else{
+                System.out.println("Numbes in num1 array are : "+no3);
+            }
         }
+    }
 
+    public static void main(String[] args) {
+        
+        int num1[] = {1,2,7,4,0,0,0};
+        int num2[] = {3,4};
+    
+        int m = num1.length;
+        int n = num2.length;
+
+        merge(num1, m, num2, n);
+        
 
     }
+
 }
