d373661b2730c04591a932728e31b21e3a3ce9c8
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-01-16 16:58:48 +0530

    update

--- LeetCodeQ2.java
+++ LeetCodeQ2.java
@@ -13,26 +13,27 @@ public class LeetCodeQ2 {
             }
         }
 
-        int nums[] = new int[length];
+        int expectedNums[] = new int[length];
 
         for(int i=0; i<length; i++){
-            nums[i]=num1[i];
-            System.out.println("Elements in nums are:- "+nums[i]);
+            expectedNums[i]=num1[i];
+            System.out.println("Elements in expectedNums are:- "+expectedNums[i]);
         }
-
+        System.out.println(expectedNums.length);
         return length;
     }
 
     public static void main(String[] args) {
         //Example 1
-        // int[] nums = {0,1,2,2,3,0,4,2};
-        // int val = 2;
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
         
         //Example 2
-        int[] nums = {3,2,2,3};
-        int val = 2;
+        // int[] nums = {3,2,2,3};
+        // int val = 2;
 
         int k = removeElement(nums, val);
-        System.out.println("Number of element after removing val from nums:- "+k);
+        
+        System.out.println("Number of element after removing val from expectedNums:- "+k);
     }
 }
