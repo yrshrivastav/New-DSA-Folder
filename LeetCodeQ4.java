1ec30f1db2b426a9f22e2421db0d70004cf18e63
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-01-29 23:10:05 +0530

    commit

--- LeetCodeQ4.java
+++ LeetCodeQ4.java
@@ -1,56 +1,37 @@
 public class LeetCodeQ4 {
 
     public static int removeDuplicates(int[] nums) {
-        int size = nums.length;
-        int count=0;
-        int twice;
+        
+        int [] result = new int[nums.length];
+        int count=0,j=0;
+        int twice=0;
+        int a=nums[0];
+        for(int i=0;i<nums.length;i++){
 
-        for (int i=0; i<size; i++)
-		{   
-            twice=0;
-                for(int j = i+1; j <size ; j++){
-                    if(nums[i]<nums[j]){
-                        nums[i]=nums[i+1];
-                        nums[i+1]=nums[j];  
-                        break;
-                    }
-                    if(nums[i]==nums[j]){
-                        twice++;
-                        if(twice==2)
-                            i++;
-                            break;
-                    }
-                }       
-		}
-        for(int i=0; i<size-1; i++){
-            if(nums[i]<nums[i+1]){
-                count++;
-                continue;
-            }
-            if(nums[i]==nums[i+1]){
-                count++;
-                continue;
+            if(a==nums[i]){
+                if(twice<2){
+                    result[j]=a;
+                    count++;
+                    j++;
+                    twice++;
+                }
             }
             else{
-                count++;
-                break;
+                twice=0;
+                a=nums[i];
+                i--;
             }
         }
 
-        for(int i=0; i<size-1; i++){
-            System.out.print(nums[i]+"\t");
+        for (int i=0;i<count;i++) {
+                System.out.print(result[i]+" ");
         }
-
-        // for(int element : nums){
-        //     System.out.print(element+"\t");
-        // }
-
         return count;
     }
     public static void main(String[] args) {
-        // int num[]={0,0,1,1,1,1,2,3,3};
-        int num[]={1,1,1,2,2,3};
-        int k=removeDuplicates(num);
+        int result[]={0,0,1,1,1,1,2,3,3};
+        // int result[]={1,1,1,2,2,3};
+        int k=removeDuplicates(result);
         System.out.println("\nSize:- "+k);
     }
 }
